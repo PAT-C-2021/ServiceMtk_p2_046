65b8b9f741bb677823339d716c40a91b00a89650
Unang <septianputratup@gmail.com>
2021-11-22 22:49:58 +0700

    Add project files.

---
+++ ServiceMtk_p2_046/Class1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMtk_p2_046
+ public class Matematika : IMatematika
+{
+    public int Bagi(int a, int b)
+    {
+        try
+        {
+            return a / b;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            //secara default
+            //throw new Exception
+
+            MathFault mf = new MathFault();
+            mf.Kode = "Inputan yang anda masukkan salah";
+            mf.Pesan = "Masukkan input yang benar";
+            throw new FaultException<MathFault>(mf);
+        }
+    }
+
+    public int Kali(int a, int b)
+    {
+        try
+        {
+            return a * b;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            //secara default
+            //throw new Exception
+
+            MathFault mf = new MathFault();
+            mf.Kode = "Inputan yang anda masukkan salah";
+            mf.Pesan = "Masukkan input yang benar";
+            throw new FaultException<MathFault>(mf);
+        }
+    }
+
+    public int Kurang(int a, int b)
+    {
+        try
+        {
+            return a - b;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            //secara default
+            //throw new Exception
+
+            MathFault mf = new MathFault();
+            mf.Kode = "Inputan yang anda masukkan salah";
+            mf.Pesan = "Masukkan input yang benar";
+            throw new FaultException<MathFault>(mf);
+        }
+    }
+
+    public int Tambah(int a, int b)
+    {
+        try
+        {
+            return a + b;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            //secara default
+            //throw new Exception
+
+            MathFault mf = new MathFault();
+            mf.Kode = "Inputan yang anda masukkan salah";
+            mf.Pesan = "Masukkan input yang benar";
+            throw new FaultException<MathFault>(mf);
+        }
+    }
+
+    //TKoordinat:nama methodnya, a,b = input seperti int.
+    //Membuat koordinat hasil lalu menjumlahkan xa dengan xb, ya dengan yb lalu dimasukkan ke hasil
+    public Koordinat TKoordinat(Koordinat a, Koordinat b)
+    {
+        try
+        {
+            Koordinat hasil = new Koordinat();
+            hasil.X = a.X + b.X;
+            hasil.Y = a.Y + b.Y;
+            return hasil;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            //secara default
+            //throw new Exception
+
+            MathFault mf = new MathFault();
+            mf.Kode = "Inputan yang anda masukkan salah";
+            mf.Pesan = "Masukkan input yang benar";
+            throw new FaultException<MathFault>(mf);
+        }
+    }
+}
+}
---
+++ ServiceMtk_p2_046/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiceMtk_p2_046
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonHasil_Click(object sender, EventArgs e)
+        {
+            int angka1 = Convert.ToInt32(textBox1.Text);
+            int angka2 = Convert.ToInt32(textBox2.Text);
+
+            double hasilTambah = obj.Tambah(angka1, angka2);
+            double hasilKurang = obj.Kurang(angka1, angka2);
+            double hasilKali = obj.Kali(angka1, angka2);
+            double hasilBagi = obj.Bagi(angka1, angka2);
+
+            labelPenjumlahan.Text = hasilTambah.ToString();
+            labelPengurangan.Text = hasilKurang.ToString();
+            labelPerkalian.Text = hasilKali.ToString();
+            labelPembagian.Text = hasilBagi.ToString();
+        }
+    }
+}
